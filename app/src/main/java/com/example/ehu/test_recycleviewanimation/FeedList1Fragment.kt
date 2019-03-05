9cdd8b43d425fb34719445af57f14c6804db69aa
ehu <fehuehuf@gmail.com>
2019-03-05 17:28:18 +0900

    fragmentのdatabinding

--- app/src/main/java/com/example/ehu/test_recycleviewanimation/FeedList1Fragment.kt
+++ app/src/main/java/com/example/ehu/test_recycleviewanimation/FeedList1Fragment.kt
@@ -1,13 +1,15 @@
 package com.example.ehu.test_recycleviewanimation
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ehu.test_recycleviewanimation.databinding.FragmentFeedList1Binding
 
 class FeedList1Fragment : Fragment() {
-
+    lateinit var binding: FragmentFeedList1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,6 +20,8 @@ class FeedList1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed_list1, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_feed_list1, container, false)
+        return binding.root
     }
 }
