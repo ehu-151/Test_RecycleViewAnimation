af62ab17dcb051c7a92d892644111a84ac18a9f0
ehu <fehuehuf@gmail.com>
2019-03-06 23:22:07 +0900

    Interpolator 実装

--- app/src/main/java/com/example/ehu/test_recycleviewanimation/FeedList1Fragment.kt
+++ app/src/main/java/com/example/ehu/test_recycleviewanimation/FeedList1Fragment.kt
@@ -9,6 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.ehu.test_recycleviewanimation.databinding.FragmentFeedList1Binding
+import android.R.anim.*
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
+import android.support.v7.widget.RecyclerView
+
+
 
 class FeedList1Fragment : Fragment() {
     lateinit var binding: FragmentFeedList1Binding
@@ -30,6 +36,16 @@ class FeedList1Fragment : Fragment() {
         val layoutManager = LinearLayoutManager(context)
         binding.recyclerview.layoutManager = layoutManager
         binding.recyclerview.adapter = DataAdapter(list)
+        binding.load.setOnClickListener { runLayoutAnimation(binding.recyclerview) }
         return binding.root
+    }
+
+    private fun runLayoutAnimation(recyclerView: RecyclerView) {
+        val context = recyclerView.context
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation_fall_down)
+
+        recyclerView.layoutAnimation = controller
+        recyclerView.adapter!!.notifyDataSetChanged()
+        recyclerView.scheduleLayoutAnimation()
     }
 }
